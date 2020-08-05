7ed97bf04917d215128e5ae422828a81b078fb97
Sangwan <jklom0326@gmail.com>
2020-08-05 19:27:49 +0900

    2020/08/05

--- src/com/example/jklom0326/chap09/section4/ExtensionCount.kt
+++ src/com/example/jklom0326/chap09/section4/ExtensionCount.kt
@@ -34,15 +34,6 @@ fun main() {
 
     val listWithNull = listOf(1, null, 3, null, 5,6)
 
-    println()
-    //filter: 식에 따라 요소를 골라내기
-    println(list.filter { it % 2 == 0 })
-    println(list.filterNot { it % 2 == 0 })
-    println(listWithNull.filterNotNull())
-
-    // filterIndexed: 인덱스와 함게 추출
-    println("filterIndexed :" + list.filterIndexed{idx, value -> idx != 1 && value % 2 == 0})
-
     // max/min : 최댓값 요소와 최솟 값 요소의 반환
     println(list.max())
     println(list.min())
@@ -54,4 +45,22 @@ fun main() {
     // sumBy: 식에 의해 도출된 모든 요소를 합산
     println(listPair.sumBy { it.second })
 
+    // map : 컬렉션에 주어진 식을 적용해 새로운 컬렉션을 반환
+    println(list.map { it * 2 }) // [2,4,6,8,10,12]
+
+    // mapIndexed: 컬렉션에 인덱스를 포함해 주어진 식을 적요해 새로운 컬렉션을 반환
+    val mapIndexed = list.mapIndexed { index, it -> index * 2}
+    println(mapIndexed) // [0,2,6,12,20,30]
+
+    // mapNotNull: null을 제외하고 식을 적용해 새로운 컬렉션 반환
+    println(listWithNull.mapNotNull { it?.times(2) }) // [2,6,10,12]
+
+    // flatMap: 각 요소에 식을 적용 후 다시 합쳐 새로운 컬렉션을 반환
+    println(list.flatMap { listOf(it, 'A') })
+    val result = listOf("abc" , "12").flatMap { it.toList() }
+    println(result)
+
+    //groupBy: 주어진 함수의 결과에 따라 그룹화 하여 map으로 반환
+    val grpMap = list.groupBy { if (it % 2 == 0 ) "even" else "odd"}
+    println(grpMap)
 }
