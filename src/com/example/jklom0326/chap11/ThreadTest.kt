2a3df9f8d77800f368a17c4e14ced1b40100b8f5
Sangwan <jklom0326@gmail.com>
2020-10-07 16:38:08 +0900

    2020-10-06

---
+++ src/com/example/jklom0326/chap11/ThreadTest.kt
@@ -0,0 +1,24 @@
+package com.example.jklom0326.chap11
+
+// 1. Thread클래스를 상속받아 구현하기
+class SimpelThread: Thread() {
+    override fun run(){
+        println("Current Threads: ${Thread.currentThread()}")
+    }
+
+    // 2. Runnable 인터페이스로부터 run() 메서드 구현하기
+    class SimpleRunnable: Runnable {
+        override fun run() {
+            println("Current Threads: ${Thread.currentThread()}")
+        }
+    }
+}
+
+fun main() {
+    val thread = SimpelThread()
+    thread.start()
+
+    val runnable = SimpelThread.SimpleRunnable()
+    val thread1 = Thread(runnable)
+    thread1.start()
+}
